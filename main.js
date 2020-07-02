e5cac5a0207c6f9d22b53de403c611da7882f160
Sevi <correosevi@gmail.com>
2020-07-02 16:02:10 +0100

    Javascript for playing the sounds added. Rain wav working

---
+++ main.js
@@ -0,0 +1,6 @@
+buttonSound = document.getElementById('myAudio');
+
+document.getElementById('playRain').addEventListener('click', function () {
+	// buttonSound.loop = true;
+	buttonSound.play();
+});
