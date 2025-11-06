7653855a7bce915739e13a9bf6ecf66f55098fc4
Sevi <correosevi@gmail.com>
2025-11-06 15:54:54 +0000

    Feat | Update audios / images / styles

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-buttonSound = document.getElementById('myAudio');
+window.addEventListener('load', () => {
+  const audios = document.querySelectorAll('.audios');
+  const playButtons = document.querySelectorAll('.play-button');
+  const pauseButtons = document.querySelectorAll('.pause-button');
 
-document.getElementById('playRain').addEventListener('click', function () {
-	// buttonSound.loop = true;
-	buttonSound.play();
+  playButtons.forEach((buttons, index) => {
+    buttons.addEventListener('click', function () {
+      pauseButtons[index].classList.remove('pause-button');
+      buttons.classList.add('hidden');
+      audios[index].play();
+    });
+  });
+
+  pauseButtons.forEach((buttons, index) => {
+    buttons.addEventListener('click', function () {
+      audios[index].pause();
+      audios[index].currentTime = 0;
+      buttons.classList.add('pause-button');
+      playButtons[index].classList.remove('hidden');
+    });
+  });
 });
